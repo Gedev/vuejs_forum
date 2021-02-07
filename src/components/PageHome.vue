df6644beaa685dad2bd203d0f712678b422b31a3
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-02-07 23:58:42 +0100

    Created Home page

---
+++ src/components/PageHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Hello from home page</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+  </div>
+</template>
+
+<script>
+import sourceData from '@data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
