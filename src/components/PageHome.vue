b6333bd9af89e1561f216e898f4126a29415dde4
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-02-08 00:18:16 +0100

    Displaying data from data.json with vue methods

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -2,15 +2,29 @@
   <div>Hello from home page</div>
   <div v-for="thread in threads" :key="thread.id">
     <h2>{{thread.title}}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import sourceData from '@data.json'
+import sourceData from '@/data.json'
 export default {
   data () {
     return {
-      threads: sourceData.threads
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(p => p.id === userId)
     }
   }
 }
